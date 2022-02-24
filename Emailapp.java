c2d8fd05bc10d42276532761be979996e746b6a3
samjayme <100197921+samjayme@users.noreply.github.com>
2022-02-24 01:01:59 +0100

    Add files via upload
    Added a method to generating random password for the user, email address using first name and last name, alternative email for a worker

---
+++ Emailapp.java
@@ -0,0 +1,16 @@
+package EmailApp;
+
+public class Emailapp {
+
+	public static void main(String[] args) {
+		Email em1 = new Email("Samuel", "Erowele");
+		em1.showInfo();
+		System.out.println(em1.showInfo());
+		
+		//em1.setAlternateEmail("samjay@gmail.com");
+		//System.out.println("Your alternate email is :" + em1.getalternateEmail());
+
+
+	}
+
+}
